cffab147010243d6b26640a64df72e7555c35ed3
Leart-Ramadani <leart.ramadani06@gmail.com>
2023-07-04 22:14:40 +0200

    Started to sort the table

--- patientSide/rezervoTermin.php
+++ patientSide/rezervoTermin.php
@@ -34,7 +34,7 @@ if (!isset($_SESSION['emri']) && !isset($_SESSION['mbiemri'])) {
     </div>
 
 
-    <div class="flex-shrink-0 p-3 text-white bg-dark sidebar">    
+    <div class="flex-shrink-0 p-3 text-white bg-dark sidebar">
         <button type="button" class="close_side"><i class="fa-solid fa-close"></i></button>
         <p class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
             <span class=" sess_admin"><?php echo $_SESSION['emri'] . ' ' . $_SESSION['mbiemri'] ?></span>
@@ -67,10 +67,43 @@ if (!isset($_SESSION['emri']) && !isset($_SESSION['mbiemri'])) {
 
 
     <?php
-    $sql = "SELECT * FROM orari";
+    $showEntries;
+    $entries = isset($_GET['entries']) ? $_GET['entries'] : 25;
+    if(isset($_GET['entries'])){
+        $showEntries = $_GET['entries'];
+        if($showEntries == 25){
+            $entry25 = 'selected';
+        } else if($showEntries == 50){
+            $entry50 = 'selected';
+        } else if($showEntries == 75){
+            $entry75 = 'selected';
+        } else if($showEntries == 100){
+            $entry100 = 'selected';
+        }
+    } 
+
+
+    $countSql = "SELECT COUNT(*) as total FROM orari";
+    $countPrep = $con->prepare($countSql);  
+    $countPrep->execute();
+    $totalRows = $countPrep->fetch();
+
+    $totalRows = $totalRows['total'];
+
+    $totalPages = ceil($totalRows / $entries);
+
+    $currentPage = isset($_GET['page']) ? $_GET['page'] : 1;
+
+    $startIndex = ($currentPage - 1 ) * $entries;
+
+
+    
+
+    $sql = "SELECT * FROM orari ORDER BY DATE(data) LIMIT :startIndex, $entries";
     $prep = $con->prepare($sql);
+    $prep->bindValue(':startIndex', $startIndex, PDO::PARAM_INT);
     $prep->execute();
-    $data = $prep->fetchAll();
+    $data = $prep->fetchAll(PDO::FETCH_ASSOC);
 
 
     if (!$data) {
@@ -79,7 +112,56 @@ if (!isset($_SESSION['emri']) && !isset($_SESSION['mbiemri'])) {
         $empty = '';
     }
     ?>
-    <main class="main mainRes">
+    <main class="main mainRes d-flex flex-column  p-2">
+        <div class="d-flex justify-content-between">
+            <div>
+                <form id="entriesForm" method="GET" class="d-flex align-items-center w-25" action="rezervoTermin.php">
+                    <label for="entries" class="me-2">Shfaq</label>
+                    <select class="form-select" id="entries" aria-label="" name="entries" style="width: 80px; height: 58px" onchange="this.form.submit()">
+                        <option value="25" <?= $entry25 ?? '' ?>>25</option>
+                        <option value="50" <?= $entry50 ?? '' ?>>50</option>
+                        <option value="75" <?= $entry75 ?? '' ?>>75</option>
+                        <option value="100"  <?= $entry100 ?? '' ?>>100</option>
+                    </select>
+                    <label for="entries" class="ms-2">rreshta</label>
+                </form>
+            </div>
+
+            <script>
+                $(document).ready(function() {
+                    $('#entries').change(function() {
+                        $('#entriesForm').submit();
+                    });
+                });
+            </script>
+
+
+            <div class="d-flex w-75 justify-content-end pe-2">
+                <div class="w-25">
+                    <form id="sortForm" method="GET" class="d-flex align-items-center w-25" action="rezervoTermin.php">
+                        <select class="form-select" id="sortBy" name="sortBy" aria-label="Default select example" style="height: 58px">
+                            <option value="ASC">Sipas renditjes A-Zh</option>
+                            <option value="DESC">Sipas renditjes Zh-A</option>
+                            <option value="date">Sipas datës</option>
+                        </select>
+                    </form>
+                </div>
+                <script>
+                    $(document).ready(function() {
+                        $('#sortBy').change(function() {
+                            $('#sortForm').submit();
+                        });
+                    });
+                </script>
+                <div class="w-50 ms-2 me-1">
+                    <div class="form-floating mb-1">
+                        <input type="text" class="form-control lastName" id="floatingInput" name="search" placeholder="Kerkro:">
+                        <label for="floatingInput">Kerko:</label>
+                        <span class="text-danger fw-normal lastNameErr"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
         <?php if ($empty == '') : ?>
             <table class="table table-striped text-center mt-2 table_patient">
                 <thead>
@@ -113,6 +195,42 @@ if (!isset($_SESSION['emri']) && !isset($_SESSION['mbiemri'])) {
                 </tbody>
             </table>
         <?php endif; ?>
+        <div class="imagePagination justify-content-end me-2">
+                <?php
+                $maxVisibleLinks = 5; // Maximum number of visible page links
+
+                $startPage = max(1, $currentPage - floor($maxVisibleLinks / 2));
+                $endPage = min($startPage + $maxVisibleLinks - 1, $totalPages);
+
+                $showEllipsisStart = ($startPage > 1);
+                $showEllipsisEnd = ($endPage < $totalPages);
+
+                if ($showEllipsisStart) {
+                    echo '<a href="?page=1" class="paginationLink">1</a>';
+                    echo '<span class="ellipsis">...</span>';
+                }
+
+                if ($currentPage > 1) {
+                    $previousPage = $currentPage - 1;
+                    echo '<a href="?page=' . $previousPage . '" class="paginationLink"><</a>';
+                }
+
+                for ($i = $startPage; $i <= $endPage; $i++) {
+                    $activePage = ($i == $currentPage) ? 'activePage' : '';
+                    echo '<a class="paginationLink ' . $activePage . '" href="?page=' . $i . '">' . $i . '</a> ';
+                }
+
+                if ($showEllipsisEnd) {
+                    echo '<span class="ellipsis">...</span>';
+                    echo '<a href="?page=' . $totalPages . '" class="paginationLink">' . $totalPages . '</a>';
+                }
+
+                if ($currentPage < $totalPages) {
+                    $nextPage = $currentPage + 1;
+                    echo '<a href="?page=' . $nextPage . '" class="paginationLink">></a>';
+                }
+                ?>
+            </div>
 
         <?php if ($empty == 'empty') : ?>
             <article style="margin-left: 200px; width: 100%;" class="mt-5">
